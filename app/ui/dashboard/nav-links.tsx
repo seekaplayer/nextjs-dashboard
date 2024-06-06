94edc50772d1f32097ff14abe7709ef6d22f1d8b
Seekaplayer <admin@seekaplayer.com>
2024-06-06 15:27:10 -0400

    fixed build issue

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -21,10 +21,10 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
-        const pathname = usePathname();
         const LinkIcon = link.icon;
         return (
           <Link
